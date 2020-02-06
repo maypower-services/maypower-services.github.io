e6d016ed88ea84ee6dd9520f1ec422fe5bcf7c5e
Erwin Flaming <erwin.flaming@gmail.com>
2020-02-07 00:16:37 +0100

    upd list view and analytics

--- js/analytics_view.js
+++ js/analytics_view.js
@@ -366,8 +366,8 @@ function update_end_date(el) {
 
 function change_page(value) {
     if (value == 'all') {
-        window.location = "/analytics_view.htm";
+        window.location = "analytics_view.htm";
     } else {
-        window.location = "/analytics_page_view.htm?id=" + value;
+        window.location = "analytics_page_view.htm?id=" + value;
     }
 }
